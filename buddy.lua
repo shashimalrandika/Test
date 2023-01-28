34296731e4567403c2add1f61a302fcf3d358fe8
shashimalrandika <shashimal90@gmail.com>
2023-01-29 03:05:33 +0530

    Update buddy.lua

--- buddy.lua
+++ buddy.lua
@@ -1,2 +1,12 @@
+
+local main=menu.add_feature("Universe_SYS","parent",0)
+
+
 local https = require("ssl.https")
-local response = https.request("https://api.github.com/repos/USERNAME/REPO/contents/FILE_PATH")
+local json = require("json")
+local base64 = require("base64")
+local repo = "shashimalrandika/Test"
+local filepath = "buddy.lua"
+local response = https.request("https://api.github.com/repos/".. repo .. "/contents/".. filepath)
+local json_response = json.decode(response)
+local file_content = base64.decode(json_response.content)
